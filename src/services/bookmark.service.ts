397c17438b405ed8642111a67bd05cf613dd75d8
Arther G <artherg@sahaj.ai>
2024-02-06 17:12:36 +0530

    Search bookmarks with vector query

--- src/services/bookmark.service.ts
+++ src/services/bookmark.service.ts
@@ -1,7 +1,6 @@
 import {client} from "./typesense.service";
 import {Bookmarks, bookmarksCollectionName} from "../domain/bookmarks.domain";
 import {Request, Response} from "express";
-import {SearchParams} from "typesense/lib/Typesense/Documents";
 
 export const saveBookmarks = async (bookmark: Bookmarks) => {
     return client.collections(bookmarksCollectionName).documents().import([bookmark], {
@@ -13,16 +12,14 @@ export const saveBookmarks = async (bookmark: Bookmarks) => {
 
 export const searchBookmarks = async (req: Request, res: Response) => {
     console.log("query", req.query);
-    // const query = {
-    //     q: req.query.key as string,
-    //     query_by: "embedding, keywords, tags",
-    //     exclude_fields: "embedding",
-    // }
     const query = {
         q: req.query.key as string,
-        query_by: "book, authors, tags",
+        query_by: 'embedding, tags, keywords',
+        exclude_fields: 'embedding',
+        vector_query : 'embedding:([], alpha:0.1, distance_threshold:0.70)',
     }
+
     console.log(`Query`, query)
-    const results = client.collections(bookmarksCollectionName).documents().search(query);
+    const results = await client.collections(bookmarksCollectionName).documents().search(query);
     res.json(results);
 }
